01c36f3fb3056198cd98c99d70d1c49449d893b3
madhudskumar <madhudskumar@live.in>
2016-07-04 21:39:15 +0530

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('starter', ['ionic'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.config(function($stateProvider){
+  $stateProvider.state('list',{
+    url:'/'
+    templateUrl:'templates/list.html'
+  })
+});
---
+++ www/js/noteCtrl.js
@@ -0,0 +1,12 @@
+app.controller('listCtrl', function ($scope) {
+  $scope.notes = [
+    {
+      title:'firstNote',
+      desc:'some notes on first Note'
+    },
+    {
+      title:'SecondNote',
+      desc:'stupid to create second'
+    }
+  ]
+});
